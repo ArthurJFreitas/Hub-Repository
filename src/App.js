130f4fe7bd7cac011d1790c5e087ad206d922331
Arthur Junio <arthu_junior@hotmail.com>
2019-11-03 18:16:55 -0200

    Create FrontEnd to list repositories of GitHub-api

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Routes />;
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 25px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    color: #666;
+    font-size: 14px;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 16px;
+    color: #7159c1;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 15px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    align-items: center;
+  }
+
+  & + li {
+    margin-top: 10px;
+  }
+
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 15px;
+
+    strong {
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #7159c1;
+        }
+      }
+
+      span {
+        background: #eee;
+        color: #333;
+        border-radius: 2px;
+        font-size: 12px;
+        font-weight: 600;
+        height: 20px;
+        padding: 3px 4px;
+        margin-left: 10px;
+      }
+    }
+
+    p {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+`;
